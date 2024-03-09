541bf32336757b1e5d9c07dcd451e9ad075bfcd2
BereketGirma <bereketgirma002@gmail.com>
2024-03-08 20:11:49 -0600

    new features in startup

---
+++ functions/startup.py
@@ -0,0 +1,12 @@
+from telebot import TeleBot
+from telebot import types
+
+def send_welcome(bot:TeleBot,message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    itembtn1 = types.KeyboardButton('Help')
+    itembtn2 = types.KeyboardButton('Option 2')
+    itembtn3 = types.KeyboardButton('Option 3')
+
+    markup.add(itembtn1,itembtn2,itembtn3)
+
+    bot.reply_to(message, "Hello and welcome to (LOB)STER\nHow can I assist you today?",reply_markup = markup)
