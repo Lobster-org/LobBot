c7e16dfedceee50777ead64f035dca5925dc2253
BereketGirma <bereketgirma002@gmail.com>
2024-03-08 20:11:13 -0600

    help function update

--- functions/help.py
+++ functions/help.py
@@ -8,7 +8,9 @@ def help_command(bot: TeleBot,message):
     "/start": "Start the bot",
     "/search": "Search for something",
     "/help": "Display available commands",
-    "/purge": "Delete messages"
+    "/purge": "Delete messages",
+    "/status": "Shares the status of a user",
+    "/rd" : "with rd you can get reddit post of a specific keyword"
     }
     
     for command, description in commands.items():
