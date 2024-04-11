a38ac9af245a98e7cf4f325ef2799cdc134182fa
BereketGirma <bereketgirma002@gmail.com>
2024-04-11 18:37:19 -0500

    Toss file
    Toss function inside file

---
+++ functions/toss.py
@@ -0,0 +1,14 @@
+
+import random
+
+def toss(bot:TeleBot,message):
+    lst = ['Heads','Tails']
+    bot.reply_to(message,random.choice(lst))
+
+def ask(bot:TeleBot,message):
+    lst = ['Yes','No','Maybe']
+    question = ' '.join(message.text.split()[1:])
+    if len(question) >= 1:
+        bot.reply_to(message, random.choice(lst))
+    else:
+        bot.reply_to(message,"Please use /ask <question> format")
