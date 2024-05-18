8feb576ae7d06d32c16d6faacdeb0f8467387a63
BereketGirma <bereketgirma002@gmail.com>
2024-05-18 15:05:26 -0500

    Update to help

--- functions/help.py
+++ functions/help.py
@@ -10,7 +10,10 @@ def help_command(bot: TeleBot,message):
     "/help": "Display available commands",
     "/purge": "Delete messages",
     "/status": "Shares the status of a user",
-    "/rd" : "with rd you can get reddit post of a specific keyword"
+    "/rd" : "with rd you can get reddit post of a specific keyword",
+    "/movie" : "Searches movies that are present on myflixertv.to",
+    "/ask" : "Randomly will answer with a yes, no or maybe",
+    "/toss" : "will do a random toss from head or tails"
     }
     
     for command, description in commands.items():
