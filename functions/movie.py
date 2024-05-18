713f46c1074176e88ff709750c88d36aef0de012
BereketGirma <bereketgirma002@gmail.com>
2024-05-18 15:05:50 -0500

    Update to movie
    Search for anime method added

--- functions/movie.py
+++ functions/movie.py
@@ -32,3 +32,34 @@ def send_movie(bot: TeleBot, message):
             bot.reply_to(message,f"Existing shows from myflixertv.to for '{movie_name}:\n{response}'")
         else:
             bot.reply_to(message,f"No shows found for {movie_name} on myflixertv.to")
+
+
+def searchAnime(anime_name):
+    url = f"https://www.crunchyroll.com/search?q={anime_name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content,"html.parser")
+        anime_title = [data_t.text.strip() for data_t in soup.findAll('h4',class_="text--gq6o- text--is-semibold--AHOYN text--is-l--iccTo search-show-card-hover__title--9ZRTG")]
+        anime = []
+        for i in range(len(anime_title)):
+            anime.append(f"{i+1}.{anime_title[i]}")
+        print(anime)
+        return anime
+    else:
+        return None
+    
+
+def send_anime(bot:TeleBot,message):
+    if len(message.text.split()) == 1:
+        bot.reply_to(message,"Please provide the name of the anime after /anime")
+    else:
+        anime_name = "%20".join(message.text.split(" ")[1:])
+        print(anime_name)
+        anime_list = searchAnime(anime_name)
+        if anime_list:
+            respone = "\n".join(anime_list)
+            bot.reply_to(message,f"Exisiting shows from crunchyroll.com '{anime_name}:\n{respone}'")
+        else:
+            bot.reply_to(message,f"No shows found for {anime_name} on crunchyroll.com :(")
+
